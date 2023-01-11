a6c387d45a493a42519bdd9031fcd697fcf04f70
lola martinhao <lola.martinhao@gmail.com>
2023-01-11 17:17:41 -0300

    fixing logo text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import styles from './Header.module.css';
-import todoLogo from '../assets/logo.svg';
+import todoLogo from '../assets/rocket.svg';
 
 export function Header() {
 
   return (
     <header className={styles.header}>
       <img src={todoLogo} alt="Logotipo do ToDo" />
+      <div className={styles.headerText}>
+        <strong className={styles.blueText}>to</strong>
+        <strong className={styles.purpleText}>do</strong>
+      </div>
     </header>
   );
 }
