a9ad6952df59243c5abc68603fe0ae41e8066586
lola martinhao <lola.martinhao@gmail.com>
2023-01-11 16:39:35 -0300

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import styles from './App.module.css';
+import { Header } from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className={styles.wrapper}>
+      <Header />
 
     </div>
   )
